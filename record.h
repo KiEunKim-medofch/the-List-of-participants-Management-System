1a67918447518a5629e897d559a2bb012e29c52c
KiEunKim-medofch <63972117+KiEunKim-medofch@users.noreply.github.com>
2020-04-22 21:18:02 +0900

    Update record.h

--- record.h
+++ record.h
@@ -7,10 +7,25 @@ typedef struct participant{
 	// TODO: define your own structure type here
   char name[30]; // the name of the participant
   char phone_num[20]; // the phone number of the participant
-  char age; // the age of the participant
+  int age; // the age of the participant
   char n_highsch[30]; // the name of the particiapant’s highschool
-  char gr_num;// the number of the group to which the participants belong
+  int gr_num;// the number of the group to which the participants belong
 
 } Participant;
 
+
+typedef struct node
+{
+  Participant pcp;
+  struct node * link;
+
+}node;
+
+typedef struct linkedList
+{
+  node *head, *tail;
+
+}linkedList;
+
 #endif
+
